64949b723c804ae2c0d3d43d7329f371a8a7cac1
Shivangi Dutta <shivangidutta4175@gmail.com>
2025-07-13 19:22:26 +0530

    Initial Commit

---
+++ HomeFlow/HomeFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeFlowApp.swift
+//  HomeFlow
+//
+//  Created by Shivangi Dutta on 13/07/25.
+//
+
+import SwiftUI
+
+@main
+struct HomeFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
